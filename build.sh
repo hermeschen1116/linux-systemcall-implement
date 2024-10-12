6dc61417b66c2499ecead36ced898e03b64526d0
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-12 19:24:09 +0800

    chore: copy necessary files to src

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+# copy modified files to source
+cp src/Makefile linux/arch/x86/kernel/
+cp src/syscall_64.tbl linux/arch/x86/entry/syscalls/
+cp src/syscalls.h linux/include/linux/
+cp src/systemcall/* linux/arch/x86/kernel/
+
+# build
 sudo make menuconfig -j8 -C linux -i linux
 sudo make arch=$(arch) -j8 -C linux -i linux
