4b34092943c0276fbdb23b781856253be5ff333c
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-13 17:58:14 +0800

    fix: build bzImage

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ cp -r src/systemcall linux/custom_systemcall
 # build
 cd linux
 sudo make menuconfig -j$process
-sudo make -j$process
+sudo make bzImage -j$process
+sudo make modules -j$process
 
 # install
 sudo make modules_install -j$process
 sudo make install -j$process
 sudo update-grub
-Reboot
+sudo reboot
