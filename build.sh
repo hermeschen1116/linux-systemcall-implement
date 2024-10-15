cf0d2657d721b5613872089be22ca44a75fbe9f4
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-15 15:07:19 +0800

    perf: use ccache with clang

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ cp src/systemcall/* linux/custom_systemcall/
 
 cd linux
 # build
-sudo make menuconfig LLVM=1 CC=clang -j$process O=./build
-sudo make LLVM=1 CC=clang Arch=$architecture -j$process O=./build
+sudo make menuconfig LLVM=1 CC="ccache clang" -j$process O=./build
+sudo make LLVM=1 CC="ccache clang" Arch=$architecture -j$process O=./build
 
 # install
 sudo make modules_install -j$process O=./build
