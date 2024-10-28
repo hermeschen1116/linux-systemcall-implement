35f32df64bcad2cff75184b90430e1cb0af3bad8
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-28 21:32:09 +0800

    fix: try to fix function

--- src/systemcall/my_get_physical_addresses.c
+++ src/systemcall/my_get_physical_addresses.c
@@ -13,40 +13,39 @@ SYSCALL_DEFINE1(my_get_physical_addresses, void *__user, user_virtual_address)
 	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
-	unsigned long *virtual_address =
-		(unsigned long *)kmalloc(sizeof(void *), GFP_KERNEL);
-	unsigned long physical_address =
-		(unsigned long *)kmalloc(sizeof(void *), GFP_KERNEL);
+	unsigned long virtual_address;
+	unsigned long physical_address;
 
 	// Copy the virtual address from user space
-	if (copy_from_user(virtual_address, user_virtual_address,
-			   sizeof(void *))) {
-		return 0;
+	if (!access_ok((void __user *)user_virtual_address, sizeof(void *))) {
+		return 0; // Invalid address
 	}
 
+	virtual_address = user_virtual_address;
+
 	// Walk the page table
-	pgd = pgd_offset(current->mm, *virtual_address);
+	pgd = pgd_offset(current->mm, virtual_address);
 	if (pgd_none(*pgd) || pgd_bad(*pgd)) {
 		return 0;
 	}
 
-	p4d = p4d_offset(pgd, *virtual_address);
+	p4d = p4d_offset(pgd, virtual_address);
 	if (p4d_none(*p4d) || p4d_bad(*p4d)) {
 		return 0;
 	}
 
-	pud = pud_offset(p4d, *virtual_address);
+	pud = pud_offset(p4d, virtual_address);
 	if (pud_none(*pud) || pud_bad(*pud)) {
 		return 0;
 	}
 
-	pmd = pmd_offset(pud, *virtual_address);
+	pmd = pmd_offset(pud, virtual_address);
 	if (pmd_none(*pmd) || pmd_bad(*pmd)) {
 		return 0;
 	}
 
 	// Find the page table entry
-	pte = pte_offset_map(pmd, *virtual_address);
+	pte = pte_offset_map(pmd, virtual_address);
 	if (!pte_present(*pte)) {
 		pte_unmap(pte);
 		return 0; // No physical address assigned
