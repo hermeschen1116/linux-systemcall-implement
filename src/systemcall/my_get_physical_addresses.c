22878467daa7299c236cd0d1411f7fc356dad8e8
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-28 19:42:25 +0800

    fix: try to fix function

--- src/systemcall/my_get_physical_addresses.c
+++ src/systemcall/my_get_physical_addresses.c
@@ -18,7 +18,7 @@ SYSCALL_DEFINE1(my_get_physical_addresses, void *, user_virtual_address)
 	unsigned long physical_address;
 
 	// Copy the virtual address from user space
-	if (copy_from_user(&virtual_address, &user_virtual_address,
+	if (copy_from_user(virtual_address, user_virtual_address,
 			   sizeof(void *))) {
 		return 0;
 	}
