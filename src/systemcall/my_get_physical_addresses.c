79ab847ee1bac497b372853eaaace0d7ff27ec42
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-28 17:02:24 +0800

    fix: correct return type of syscall

--- src/systemcall/my_get_physical_addresses.c
+++ src/systemcall/my_get_physical_addresses.c
@@ -15,12 +15,13 @@ SYSCALL_DEFINE1(my_get_physical_addresses, void *, user_virtual_address)
 	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
-	unsigned long physical_address;
+	unsigned long *physical_address;
 
 	// Copy the virtual address from user space
 	if (copy_from_user(&virtual_address, &user_virtual_address,
-			   sizeof(void *)))
+			   sizeof(void *))) {
 		return 0;
+	}
 
 	// Get the current process's memory map
 	mm = current->mm;
