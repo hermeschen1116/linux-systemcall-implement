056ce9e84032c13eeca4c08718e78a6de6671702
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-28 19:17:11 +0800

    fix: remove unused comma

--- src/test_programs/project1/question2.c
+++ src/test_programs/project1/question2.c
@@ -12,12 +12,12 @@ int main()
 	void *phy_add;
 
 	phy_add = my_get_physical_addresses(&a[0]);
-	printf("global element a[0]:\n", );
+	printf("global element a[0]:\n");
 	printf("Offest of logical address:[%p]   Physical address:[%p]\n",
 	       &a[0], phy_add);
 	printf("========================================================================\n");
 	phy_add = my_get_physical_addresses(&a[1999999]);
-	printf("global element a[1999999]:\n", );
+	printf("global element a[1999999]:\n");
 	printf("Offest of logical address:[%p]   Physical address:[%p]\n",
 	       &a[1999999], phy_add);
 	printf("========================================================================\n");
