e189dc4ed185b6868d869a8090c0592f27959e70
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-29 20:10:56 +0800

    perf: merge debug info

--- src/systemcall/my_get_physical_addresses.c
+++ src/systemcall/my_get_physical_addresses.c
@@ -49,12 +49,9 @@ SYSCALL_DEFINE1(my_get_physical_addresses, void *, user_virtual_address)
 	page_frame_number = pte_val(*pte) & PAGE_MASK;
 	page_offset = virtual_address & ~PAGE_MASK;
 	physical_address = page_frame_number | page_offset;
-	printk(KERN_DEBUG "my_get_physical_addresses:\nvirtual address: %lx\n",
-	       virtual_address);
 	printk(KERN_DEBUG
-	       "my_get_physical_addresses:\npage frame number: %lx, page offset: %lx\n",
-	       (page_frame_number >> PAGE_SHIFT), page_offset);
-	printk(KERN_DEBUG "my_get_physical_addresses:\nphysical address: %lx\n",
+	       "my_get_physical_addresses:\nvirtual address: %lx\npage frame number: %lx, page offset: %lx\nphysical address: %lx\n",
+	       virtual_address, (page_frame_number >> PAGE_SHIFT), page_offset,
 	       physical_address);
 
 	return physical_address;
