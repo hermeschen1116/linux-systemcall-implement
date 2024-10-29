7f7fd54a028548e8b7662f1f188a079fbbd496d0
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-29 08:47:25 +0800

    WIP

--- src/systemcall/my_get_physical_addresses.c
+++ src/systemcall/my_get_physical_addresses.c
@@ -1,53 +1,76 @@
 #include <linux/syscalls.h>
+// #define DEBUG
 
-SYSCALL_DEFINE1(my_get_physical_addresses, void *, user_virtual_address)
+SYSCALL_DEFINE1(my_get_physical_addresses, void *, addr_p)
 {
-	unsigned long virtual_address = (unsigned long)user_virtual_address;
+	unsigned long vaddr = (unsigned long)addr_p;
 	pgd_t *pgd;
 	p4d_t *p4d;
 	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
-	unsigned long page_frame_number = 0;
-	unsigned long offset = 0;
-	unsigned long physical_address = 0;
+	unsigned long paddr = 0;
+	unsigned long page_addr = 0;
+	unsigned long page_offset = 0;
 
-	// Walk the page table
-	pgd = pgd_offset(current->mm, virtual_address);
-	if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd))) {
-		printk(KERN_WARNING "my_get_physical_addresses: Invalid PGD\n");
+	pgd = pgd_offset(current->mm, vaddr);
+#ifdef DEBUG
+	printk("pgd_val = 0x%lx\n", pgd_val(*pgd));
+	printk("pgd_index = %lu\n", pgd_index(vaddr));
+#endif
+	if (pgd_none(*pgd)) {
+		printk("not mapped in pgd\n");
 		return 0;
 	}
 
-	p4d = p4d_offset(pgd, virtual_address);
-	if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d))) {
-		printk(KERN_WARNING "my_get_physical_addresses: Invalid P$D\n");
+	p4d = p4d_offset(pgd, vaddr);
+#ifdef DEBUG
+	printk("p4d_val = 0x%lx\n", p4d_val(*p4d));
+	printk("p4d_index = %lu\n", p4d_index(vaddr));
+#endif
+	if (p4d_none(*p4d)) {
+		printk("not mapped in p4d\n");
 		return 0;
 	}
 
-	pud = pud_offset(p4d, virtual_address);
-	if (pud_none(*pud) || unlikely(pud_bad(*pud))) {
-		printk(KERN_WARNING "my_get_physical_addresses: Invalid PUD\n");
+	pud = pud_offset(p4d, vaddr);
+#ifdef DEBUG
+	printk("pud_val = 0x%lx\n", pud_val(*pud));
+	printk("pud_index = %lu\n", pud_index(vaddr));
+#endif
+	if (pud_none(*pud)) {
+		printk("not mapped in pud\n");
 		return 0;
 	}
 
-	pmd = pmd_offset(pud, virtual_address);
-	if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd))) {
-		printk(KERN_WARNING "my_get_physical_addresses: Invalid PMD\n");
+	pmd = pmd_offset(pud, vaddr);
+#ifdef DEBUG
+	printk("pmd_val = 0x%lx\n", pmd_val(*pmd));
+	printk("pmd_index = %lu\n", pmd_index(vaddr));
+#endif
+	if (pmd_none(*pmd)) {
+		printk("not mapped in pmd\n");
 		return 0;
 	}
 
-	// Find the page table entry
-	pte = pte_offset_kernel(pmd, virtual_address);
+	pte = pte_offset_kernel(pmd, vaddr);
+#ifdef DEBUG
+	printk("pte_val = 0x%lx\n", pte_val(*pte));
+	printk("pte_index = %lu\n", pte_index(vaddr));
+#endif
 	if (pte_none(*pte)) {
-		printk(KERN_WARNING
-		       "my_get_physical_addresses: No PTE found\n");
+		printk("not mapped in pte\n");
 		return 0;
 	}
 
-	page_frame_number = pte_pfn(*pte);
-	offset = virtual_address & ~PAGE_MASK;
-	physical_address = (page_frame_number << PAGE_SHIFT) | offset;
+	/* Page frame physical address mechanism | offset */
+	page_addr = pte_val(*pte) & PAGE_MASK;
+	page_offset = vaddr & ~PAGE_MASK;
+	paddr = page_addr | page_offset;
+#ifdef DEBUG
+	printk("page_addr = %lx, page_offset = %lx\n", page_addr, page_offset);
+	printk("vaddr = %lx, paddr = %lx\n", vaddr, paddr);
+#endif
 
-	return physical_address;
+	return paddr;
 }
