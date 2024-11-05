5d5772bf96cdb9d7da7fc1136ff78effdd5d8adb
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-06 00:49:04 +0800

    refactor: restore change

--- src/test_programs/project1/question1.c
+++ src/test_programs/project1/question1.c
@@ -19,8 +19,8 @@ int main()
 	*shared_memory = 48763;
 
 	printf("Initial value in parent process: %d\n", *shared_memory);
-	printf("Parent's physical address before fork: [0x%p]\n",
-	       (void *)my_get_physical_addresses(shared_memory));
+	printf("Parent's physical address before fork: [0x%lx]\n",
+	       my_get_physical_addresses(shared_memory));
 	unsigned long parent_physical_address_before =
 		my_get_physical_addresses(shared_memory);
 
@@ -29,21 +29,21 @@ int main()
 		perror("fork failed");
 		exit(1);
 	} else if (pid == 0) {
-		printf("Child's physical address before write: [0x%p]\n",
-		       (void *)my_get_physical_addresses(shared_memory));
+		printf("Child's physical address before write: [0x%lx]\n",
+		       my_get_physical_addresses(shared_memory));
 
 		*shared_memory = 114514;
 		printf("New value in child process: %d\n", *shared_memory);
 
-		printf("Child's physical address after write: [0x%p]\n",
-		       (void *)my_get_physical_addresses(shared_memory));
+		printf("Child's physical address after write: [0x%lx]\n",
+		       my_get_physical_addresses(shared_memory));
 
 		exit(0);
 	} else {
 		wait(NULL);
 
-		printf("Parent's physical address after child's write: [0x%p]\n",
-		       (void *)my_get_physical_addresses(shared_memory));
+		printf("Parent's physical address after child's write: [0x%lx]\n",
+		       my_get_physical_addresses(shared_memory));
 		unsigned long parent_physical_address_after =
 			my_get_physical_addresses(shared_memory);
 
